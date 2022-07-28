e009e952f367d83a39311e17418904f8bf0471f0
alexander <alexanderciriloramirezluna@gmail.com>
2022-07-28 18:07:02 -0500

    clase dto animal consulta

---
+++ src/main/java/com/vetapp/dto/AnimalDetalleDto.java
@@ -0,0 +1,178 @@
+package com.vetapp.dto;
+
+public class AnimalDetalleDto {
+	
+	 private Long id;
+
+	 private String arete;
+	 
+	 private String nombre;
+	 
+	 private String raza;
+	 
+	 private String categoriareproduccion;
+	 
+	 private String categoriaproduccion;
+	 
+	 private String sexo;
+	 
+	 private String estadoproductivo;
+	 
+	 private String estadoreproductivo;
+	 
+	 private String corral;
+	 
+	 private String fechanacimiento;
+	 
+	 private String fechaultimoparto;
+	 
+	 private String numeroparto;
+	 
+	 private String padre;
+	 
+	 private String madre;
+	 
+	 private String precio;
+	 
+	 private String venta;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getArete() {
+		return arete;
+	}
+
+	public void setArete(String arete) {
+		this.arete = arete;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getRaza() {
+		return raza;
+	}
+
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+
+	public String getCategoriareproduccion() {
+		return categoriareproduccion;
+	}
+
+	public void setCategoriareproduccion(String categoriareproduccion) {
+		this.categoriareproduccion = categoriareproduccion;
+	}
+
+	public String getCategoriaproduccion() {
+		return categoriaproduccion;
+	}
+
+	public void setCategoriaproduccion(String categoriaproduccion) {
+		this.categoriaproduccion = categoriaproduccion;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getEstadoproductivo() {
+		return estadoproductivo;
+	}
+
+	public void setEstadoproductivo(String estadoproductivo) {
+		this.estadoproductivo = estadoproductivo;
+	}
+
+	public String getEstadoreproductivo() {
+		return estadoreproductivo;
+	}
+
+	public void setEstadoreproductivo(String estadoreproductivo) {
+		this.estadoreproductivo = estadoreproductivo;
+	}
+
+	public String getCorral() {
+		return corral;
+	}
+
+	public void setCorral(String corral) {
+		this.corral = corral;
+	}
+
+	public String getFechanacimiento() {
+		return fechanacimiento;
+	}
+
+	public void setFechanacimiento(String fechanacimiento) {
+		this.fechanacimiento = fechanacimiento;
+	}
+
+	public String getFechaultimoparto() {
+		return fechaultimoparto;
+	}
+
+	public void setFechaultimoparto(String fechaultimoparto) {
+		this.fechaultimoparto = fechaultimoparto;
+	}
+
+	public String getNumeroparto() {
+		return numeroparto;
+	}
+
+	public void setNumeroparto(String numeroparto) {
+		this.numeroparto = numeroparto;
+	}
+
+	public String getPadre() {
+		return padre;
+	}
+
+	public void setPadre(String padre) {
+		this.padre = padre;
+	}
+
+	public String getMadre() {
+		return madre;
+	}
+
+	public void setMadre(String madre) {
+		this.madre = madre;
+	}
+
+	public String getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+
+	public String getVenta() {
+		return venta;
+	}
+
+	public void setVenta(String venta) {
+		this.venta = venta;
+	}
+	 
+	 
+	 
+
+}
